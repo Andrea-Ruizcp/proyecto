01c42ed6dc993590e3da4de0a0fbe7fc1316a592
Andrea-Ruizcp <andrea.ruiz.ttc.218@unilibre.edu.co>
2024-12-01 23:13:59 -0500

    Update newshoes.php

--- partes/newshoes.php
+++ partes/newshoes.php
@@ -66,7 +66,6 @@
             <button id="next-btn" class="slider-btn"><i class="fa-solid fa-chevron-right"></i></button>
         </div>
     </main>
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="http://localhost/comercio/proyecto/script/newshoes.js"></script>
 </body>
