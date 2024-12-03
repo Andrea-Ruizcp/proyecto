f4e349860e3916ab60e771cdf866999080c4a306
JGonzalezGarcia1 <127920364+JGonzalezGarcia1@users.noreply.github.com>
2024-12-02 23:15:29 -0500

    Cambio en login

--- navegacion/registro.php
+++ navegacion/registro.php
@@ -4,6 +4,16 @@ include("../conexion/conexion.php");
 
 include("../include/header.php");
 
+    $nombre_completo = $_POST["nombre"];
+    $cedula = $_POST["cedula"];
+    $direccion = $_POST["direccion"];
+    $correo_electronico = $_POST["correo"];
+    $contrasena = $_POST["contraseña"];
+
+    $consulta = "INSERT INTO usuarios(nombre, cedula, direccion, correo, contraseña)
+                VALUES ('$nombre', '$cedula', '$direccion', '$correo', '$contraseña')";
+    
+    $ejecutar = mysqli_query($conexion, $consulta);
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +29,7 @@ include("../include/header.php");
     <link rel="shortcut icon" href="/image/logo.png" type="image/x-icon">
 </head>
 <body>
-
+<form action="" method="">
 <div class="content">
         <div class="contenedores bg-white p-5 text-secondary">
             <div class=" texto text-center fs-2 fw-light">Registrarse</div>
@@ -28,22 +38,27 @@ include("../include/header.php");
                 <a href="http://localhost/comercio/proyecto/navegacion/login.php" class="text-decoration-none fw-semibold" style="color: #ffdc41;">Inicia sesión</a>
             </div>
             <div class="input-group mt-4">
-                <input class="form-control bg-white" type="text" placeholder="Nombre completo" />
+                <input class="form-control bg-white" type="text" placeholder="Nombre completo" name="nombre"/>
             </div>
             <div class="input-group mt-3">
-                <input class="form-control bg-white" type="email" placeholder="Correo electrónico" />
+                <input class="form-control bg-white" type="text" placeholder="Cedula" name="cedula"/>
             </div>
             <div class="input-group mt-3">
-                <input class="form-control bg-white" type="password" placeholder="Contraseña" />
+                <input class="form-control bg-white" type="text" placeholder="Dirección" name="direccion" />
             </div>
             <div class="input-group mt-3">
-                <input class="form-control bg-white" type="password" placeholder="Confirmar contraseña" />
+                <input class="form-control bg-white" type="email" placeholder="Correo electrónico" name="correo"/>
             </div>
+            <div class="input-group mt-3">
+                <input class="form-control bg-white" type="password" placeholder="Contraseña" name="contrasena"/>
+            </div>
+           
             <div class="btn text-white w-100 mt-4 fw-semibold shadow-sm" style="background-color: #ffdc41;">
                 <a href="http://localhost/comercio/proyecto/principal/" class="text-decoration-none text-white">Registrarse</a>
             </div>
         </div>
     </div>
+</form>
   
     <?php include("../include/footer.php"); ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
